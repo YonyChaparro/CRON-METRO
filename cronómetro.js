96a407e586c5efe55a5160a95e7ab3cd1355af23
yonyChaparro <ychaparro@unal.edu.co>
2023-03-20 23:17:00 -0500

    v2.0

--- cronómetro.js
+++ cronómetro.js
@@ -1,1 +1,25 @@
-console.log("Junciona")
+function cronometro(){
+    var seg=0, min=0, hor=0
+    
+
+    while (hor<1) {
+
+        if (seg<59) {
+            seg++;
+        }else{
+            seg=0
+            min++
+            if (min<59) {
+                min++
+            }else{
+                min=0
+                hor++
+            }
+        }
+        
+         console.log(hor, min, seg)
+    }
+}
+
+
+cronometro();
